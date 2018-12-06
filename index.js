09a9c77c5d6beb620fe5fdc2b03b8b3da4127960
John Arnone Jr <johnsarnonejr@gmail.com>
2018-12-06 01:50:48 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+var uppercase
+var lowercase
+
+function shout(string) {
+  uppercase = string.toUpperCase()
+  string = uppercase
+  return 'hello'.toUpperCase()
+}
+
+function whisper(string) {
+  return 'HELLO'.toLowerCase()
+}
+function logShout(shout) {
+ return console.log('hello'.toUpperCase()); 
+}
+
+function logWhisper(whisper) {
+  return console.log('HELLO'.toLowerCase());
+}
+
+const sayHiToGrandma = (uppercase, lowercase, mixedcase) => {
+  if uppercase
+}
